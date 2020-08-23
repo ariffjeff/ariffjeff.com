7ed6dd69f6d5ee67a2e18cd5ad6c974204eb667b
Ariff <ariffjeff@icloud.com>
2020-08-23 17:09:19 -0400

    index, about, resume

--- js/main.js
+++ js/main.js
@@ -1,16 +1,52 @@
 function copyElemText(id) {
-    var text = document.getElementById(id).textContent
-    var textArea = document.createElement("textarea");
-    elem = document.body.appendChild(textArea);
-    textArea.value = text;
-    textArea.select();
-    document.execCommand("copy");
-    elem.remove()
+  var p = document.getElementById(id);
+  var text = p.textContent;
+  var textArea = document.createElement("textarea");
+  elem = document.body.appendChild(textArea);
+  textArea.value = text;
+  textArea.select();
+  document.execCommand("copy");
+  elem.remove();
+  
+  // p.style.backgroundColor = 'blue';
+  
+  var transitionEnd = whichTransitionEvent();
+  
+  p.classList.add('anim-copied');
+  // $(this).one(transitionEnd, p.classList.remove('copied-anim'))
+  setTimeout(() => {  
+    p.classList.remove('anim-copied');
+  }, 150); // same time as animation in class
+  // https://jonsuh.com/blog/detect-the-end-of-css-animations-and-transitions-with-javascript/
+  // https://www.sitepoint.com/delay-sleep-pause-wait/
 }
 
-// function copyElementText(id) {
-//     var p = document.getElementById(id);
-//     p.select();
-//     document.execCommand('copy');
-//     alert("Copied the text: " + text);
-// }
+// browser specific transition type
+function whichTransitionEvent(){
+  var t;
+  var el = document.createElement('fakeelement');
+  var transitions = {
+    'transition':'transitionend',
+    'OTransition':'oTransitionEnd',
+    'MozTransition':'transitionend',
+    'WebkitTransition':'webkitTransitionEnd'
+  }
+  
+  for(t in transitions){
+    if( el.style[t] !== undefined ){
+      return transitions[t];
+    }
+  }
+}
+
+// play video on hover
+$(document).ready(function(){
+  var vid = $('.vidLink').children();
+  vid.on('mouseenter focus', function() {
+    vid.get(0).play();
+  })
+  vid.on('mouseout blur', function() {
+    vid.get(0).pause();
+  })
+});
+
