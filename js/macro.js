af9ee83975843bf7c90895e1117bfba38ddb8655
Ariff <ariffjeff@icloud.com>
2020-09-05 19:19:33 -0400

    add macro (fixed)
    Necessary requirejs command to dynamically load images into html doesn't work without nodejs which Github Pages doesn't support, so instead... Dynamically load in images from folder in dev, copy resulting html from browser inspect element, paste into html file, comment out js.

--- js/macro.js
+++ js/macro.js
@@ -1,23 +1,32 @@
-$(document).ready(function() {
-  var folder = "img/macro";
-  $.ajax({
-    url : folder,
-    success: function (data) {
-      // add images to html
-      $(data).find("a").attr("href", function (i, val) {
-        if( val.match(/\.(jpe?g|png|gif|webp)$/) ) { 
-          var filename = val.replace(/^.*[\\\/]/, '')
-          $("#macroGrid").append("\
-          <a href='" + folder + "/high/" + filename + "'>\
-          <picture>\
-          <source type='image/webp' srcset='" + val + "'/>\
-          <source type='image/jpg' srcset='" + val + "'/>\
-          <img src='" + val + "' alt='" + filename + "'>\
-          </picture>\
-          </a>\
-          ");
-        }
-      });
-    }
-  });
-});
+// loads images from local dev dir. Copy created html from Inspect Element, paste into html file.
+// REMEMBER TO UNCOMMENT <script> TAG IN HTML
+// $(document).ready(function() {
+//   var folder = "img/macro";
+//   $.ajax({
+//     url : folder,
+//     success: function (data) {
+//       // add images to html
+//       $(data).find("a").attr("href", function (i, val) {
+//         if( val.match(/\.(jpe?g|png|gif|webp)$/) ) { 
+//           var filename = val.replace(/^.*[\\\/]/, '')
+//           $("#macroGrid").append("\
+//           <a href='" + folder + "/high/" + filename + "'>\
+//           <picture>\
+//           <source type='image/webp' srcset='" + val + "'/>\
+//           <source type='image/jpg' srcset='" + val + "'/>\
+//           <img src='" + val + "' alt='" + filename + "'>\
+//           </picture>\
+//           </a>\
+//           ");
+//         }
+//       });
+//     }
+//   });
+// });
+
+// only works with node.js 
+// var moduleName = 'require';
+// require([moduleName], function(fs){
+//     // var files = fs.readdirSync('/img/macro/');
+//     alert(files)
+// })
