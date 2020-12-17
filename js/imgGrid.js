45ca43631ccfe0678426507712eaee353dd0679d
Ariff <ariffjeff@icloud.com>
2020-12-16 21:15:33 -0500

    new grid layout, add design imgs

---
+++ js/imgGrid.js
@@ -0,0 +1,33 @@
+// loads images from local dev dir. Copy created html from Inspect Element, paste into html file.
+// REMEMBER TO UNCOMMENT <script> TAG IN HTML
+$(document).ready(function() {
+  var folder = "img/design";
+  $.ajax({
+    url : folder,
+    success: function (data) {
+      // add images to html
+      $(data).find("a").attr("href", function (i, val) {
+        if( val.match(/\.(jpe?g|png|gif|webp)$/) ) { 
+          var filename = val.replace(/^.*[\\\/]/, '')
+          $("#imgGrid").append("\
+          <a href='" + folder + "/" + filename + "'>\
+          <picture>\
+          <source type='image/webp' srcset='" + val + "'/>\
+          <source type='image/jpg' srcset='" + val + "'/>\
+          <img src='" + val + "' alt='" + filename + "'>\
+          </picture>\
+          </a>\
+          ");
+        }
+      });
+    }
+  });
+});
+
+// only works with node.js 
+// https://stackoverflow.com/questions/31274329/get-list-of-filenames-in-folder-with-javascript
+// var moduleName = 'require';
+// require([moduleName], function(fs){
+//     // var files = fs.readdirSync('/img/macro/');
+//     alert(files)
+// })
