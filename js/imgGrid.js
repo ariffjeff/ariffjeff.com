e6b6b8a04fdad5a2978c66c278c90428096500ed
Ariff <ariffjeff@icloud.com>
2020-12-18 23:11:59 -0500

    add webp, removed unecessary webp html
    html created from poorly written but working js. js assumes img dir has either jpg only, or jpg + webp versions of images. webp only would work but html wouldn't be completely correct.

--- js/imgGrid.js
+++ js/imgGrid.js
@@ -1,25 +1,46 @@
 // loads images from local dev dir. Copy created html from Inspect Element, paste into html file.
 // REMEMBER TO UNCOMMENT <script> TAG IN HTML
-$(document).ready(function() {
+$(function() {
   var folder = "img/design";
   $.ajax({
     url : folder,
     success: function (data) {
       // add images to html
-      $(data).find("a").attr("href", function (i, val) {
-        if( val.match(/\.(jpe?g|png|gif|webp)$/) ) { 
-          var filename = val.replace(/^.*[\\\/]/, '')
-          $("#imgGrid").append("\
-          <a href='" + folder + "/" + filename + "'>\
-          <picture>\
-          <source type='image/webp' srcset='" + val + "'/>\
-          <source type='image/jpg' srcset='" + val + "'/>\
-          <img src='" + val + "' alt='" + filename + "'>\
-          </picture>\
-          </a>\
-          ");
+      // data in this case is the entire html doc of the location of folder
+      // val: dir + filename + ext
+      var files = []
+      $(data).find("a").attr("href", function (i, val) { // loop over all img dir anchors
+        if(val.match(/\.(jpe?g|png|gif|webp)$/)) {
+          files.push(val.replace(/^.*[\\\/]/, ''))
         }
       });
+      
+      for(var i = 0; i < files.length;) {
+        var source = "<source type='image/jpg' srcset='" + folder + "/" + files[i] + "'/>"
+        var filename0 = files[i].replace(/\.[^/.]+$/, '')
+        var filename1 = files[i+1].replace(/\.[^/.]+$/, '')
+        var filename_priority = files[i] // jpg or webp
+        if(filename0 == filename1) {
+          source = "\
+          <source type='image/webp' srcset='" + folder + "/" + files[i+1] + "'/>\
+          <source type='image/jpg' srcset='" + folder + "/" + files[i] + "'/>\
+          "
+          filename_priority = files[i+1]
+          i+=2
+        } else {
+          i++
+        }
+        
+        $("#imgGrid").append("\
+        <a href='" + folder + "/" + filename_priority + "'>\
+        <picture>"
+        + source + 
+        "<img src='" + folder + "/" + filename_priority + "' alt='" + filename_priority + "'>\
+        </picture>\
+        </a>\
+        ");
+        
+      }
     }
   });
 });
